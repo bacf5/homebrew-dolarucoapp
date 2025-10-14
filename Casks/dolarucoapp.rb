15c0ac08c5913f5e5d6f5ea0d0b97dd5b004a98d
Bruno <73502522+bacf5@users.noreply.github.com>
2025-10-14 16:25:41 +0200

    Created: dolarucoapp.rb formula cask

---
+++ Casks/dolarucoapp.rb
@@ -0,0 +1,12 @@
+cask "dolarucoapp" do
+  version "1.0.0"
+  sha256 "0173602a39d123ef29868100b4fdc3253620027fcf17cde8fb9a34ddef7bae0d"
+
+  url "https://github.com/bacf5/homebrew-dolarucoapp/releases/download/v1.0/dolaruco-app.zip"
+  name "DolarucoApp"
+  desc "USD currency checker for argentinians living abroad."
+  homepage "https://github.com/bacf5/dolaruco-app"
+
+  app "dolaruco-app.app" 
+  
+end

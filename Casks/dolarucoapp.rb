50d32c3f5be9fee53755819999ccbb82dced8038
Bruno <73502522+bacf5@users.noreply.github.com>
2025-10-17 00:58:15 +0200

    Update dolaruco-app to version 1.1

--- Casks/dolarucoapp.rb
+++ Casks/dolarucoapp.rb
@@ -1,11 +1,16 @@
 cask "dolarucoapp" do
-  version "1.0.0"
-  sha256 "0173602a39d123ef29868100b4fdc3253620027fcf17cde8fb9a34ddef7bae0d"
+  version "1.1"
+  sha256 "22c92fee0a0e67a7dd7616cd40bf5c401291a855f20ffc49a5df28bc964b9977"
 
-  url "https://github.com/bacf5/homebrew-dolarucoapp/releases/download/v1.0/dolaruco-app.zip"
+  url "https://github.com/bacf5/dolaruco-app/releases/download/v1.1/dolaruco-app.zip"
   name "DolarucoApp"
   desc "USD currency checker for argentinians living abroad."
   homepage "https://github.com/bacf5/dolaruco-app"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
 
   app "dolaruco-app.app" 
   
